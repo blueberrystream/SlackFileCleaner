b0dd099595b0928fa2e14c19855a89193afc3c8b
saitota <1152469+saitota@users.noreply.github.com>
2018-03-22 22:28:19 +0900

    1st commit

---
+++ README.md
@@ -0,0 +1,50 @@
+﻿# 🗑️ Slack File Cleaner
+It is a serverless function that periodically delete old attatchment files.
+
+## Description
+Using Slack's LEGACY TOKEN to check old files, and delete them all.
+LEGACY TOKEN needs to be create manually, but AWS side automates environment construction by using Serverless Framework.
+
+## Requirement
+- AWS Account
+- Serverless Framework
+- [serverless-plugin-aws-alerts](https://serverless.com/blog/serverless-ops-metrics/) (optional)
+- Slack Account / Administrator Role
+
+## Installation
+1. Create LEGACY TOKEN from [Here](https://api.slack.com/custom-integrations/legacy-tokens)
+
+2. Clone this repo.
+```
+$ git clone https://github.com/saitota/SlackFileCleaner.git
+```
+
+4. Modify environment_dev.yml 's two TOKEN to your token.
+``` environment_dev.yml
+LEGACY_TOKEN: 'xoxp-000000000000-000000000000-000000000000-0x0x0x0x0x0x0x0x0x0x0x0x0x0x0x0x'
+```
+
+5. Deploy with Serverless Framework (you must aws-cli initialize before)
+```
+$ sls deploy ./SlackFileCleaner
+...
+api keys:
+  None
+endpoints:
+  None
+functions:
+  fnc: SlackFileCleaner-dev-fnc
+```
+6. Done! Wait a time to archive. (12:30 JST is the default)
+
+# 🤔 Anything Else
+I will write article about this function.
+
+[saitotak - Qiita](https://qiita.com/saitotak/)
+
+# 🐑 Author
+[saitotak](https://qiita.com/saitotak)
+
+# ✍ License
+[MIT](./LICENSE)
+
---
+++ main.py
@@ -0,0 +1,73 @@
+import json
+import logging
+import urllib.request
+import os
+from datetime import datetime
+import time
+
+print('Loading function... ')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+class PostJson(object):
+    def __init__(self):
+        self.LEGACY_TOKEN = os.environ['LEGACY_TOKEN']
+    def headers(self):
+        return {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Authorization': 'Bearer {0}'.format(self.LEGACY_TOKEN)
+        }
+    def file_list(self,page):
+        return {
+            'token': self.LEGACY_TOKEN,
+            'count': '100',
+            'page': page
+        }
+    def file_delete(self,file):
+        return {
+            'token': self.LEGACY_TOKEN,
+            'file': file
+        }
+
+def handler(event, context):
+    CLEAN_AFTER_DAYS =os.environ['CLEAN_AFTER_DAYS']
+    # list all files / you can only get 100 files/1 page at files.list
+    for cnt_page in range(1,100):
+        url = 'https://slack.com/api/files.list'
+        post_body = PostJson().file_list(cnt_page)
+        req = urllib.request.Request(
+            url,
+            urllib.parse.urlencode(post_body).encode('utf-8'))
+        res = urllib.request.urlopen(req)
+        files = json.loads(res.read().decode('utf-8'))
+        # get files info
+        for file in files.get('files'):
+            logger.info('check file timestamp: %s ', file.get('id', ''))
+            # get timestamp of the file
+            ts = file.get('timestamp',
+                                '1000000000')  # epoch 2001/9/9 10:46:40 if blank
+            ts_datetime = datetime.fromtimestamp(float(ts))
+            now_datetime = datetime.now()
+            diff_datetime = now_datetime - ts_datetime
+            # check old file
+            if (int(diff_datetime.days) > int(CLEAN_AFTER_DAYS)):
+                logger.info('delete file: %s', file.get('id', ''))
+                # Delete files / require scope : Legacy-token & Administrator role
+                post_data = PostJson().file_delete(file.get('id', ''))
+                post_head = PostJson().headers()
+                url = 'https://slack.com/api/files.delete'
+                req = urllib.request.Request(
+                    url,
+                    data=json.dumps(post_data).encode('utf-8'),
+                    method='POST',
+                    headers=post_head)
+                res = urllib.request.urlopen(req)
+                #tmp = json.loads(res.read().decode('utf8')) #dbg
+                #print (tmp) #dbg
+                logger.info('message result: %s', res.msg)
+        # exit if all files are checked
+        if len(files.get('files',0)) < 100:
+            print('done! :' + str(len(files.get('files',0))))
+            break
+    return 'ok'
